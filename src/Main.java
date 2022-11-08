7e3d299d330ff1c995da363b6466863e144704a1
mucansu <mucansu@gmail.com>
2022-11-08 16:33:22 +0100

    Change1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("change1");
+        System.out.println("Change Github");
+        System.out.println("Change Github2");
 
     }
 }
