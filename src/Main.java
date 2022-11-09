e54979d0b89efeddca3f3050a96f8f7ca6d0a309
mucansu <mucansu@gmail.com>
2022-11-09 10:33:06 +0100

    X

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("change1");
+
         System.out.println("Change Github");
-        System.out.println("Change Github2");
-System.out.println("test1");
+        System.out.println("Change Github");
+        System.out.println("Change Github");
 
     }
 }
